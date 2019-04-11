6fa70cb0bbcea70382fd881d36e45c8763b3dc59
Тулупов Иван Васильевич <C:\The Bat!>
2019-04-11 12:35:14 +0300

    First commit

---
+++ Level09/task0922/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task09.task0922;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/*
+Ввести с клавиатуры дату в формате "2013-08-18"
+Вывести на экран введенную дату в виде "AUG 18, 2013".
+Воспользоваться объектом Date и SimpleDateFormat.
+
+Какое сегодня число?
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = dt.parse(reader.readLine());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy", Locale.ENGLISH);
+        System.out.println(sdf.format(date).toUpperCase());
+    }
+}
