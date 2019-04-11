55e0f216cbc21a6422cd02b0469524798d66390d
Тулупов Иван Васильевич <C:\The Bat!>
2019-04-11 12:34:22 +0300

    First commit

---
+++ Level09/task0923/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task09.task0923;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Гласные и согласные
+*/
+
+public class Solution {
+    public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine().replaceAll("\\s+", "");
+        String stG = "", stS = "";
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (isVowel(str.charAt(i)))
+            {
+                stG += str.charAt(i) + " ";
+            }
+            else
+            {
+                stS += str.charAt(i) + " ";
+            }
+        }
+        System.out.println(stG);
+        System.out.println(stS);
+    }
+
+    // метод проверяет, гласная ли буква
+    public static boolean isVowel(char c) {
+        c = Character.toLowerCase(c);  // приводим символ в нижний регистр - от заглавных к строчным буквам
+
+        for (char d : vowels)   // ищем среди массива гласных
+        {
+            if (c == d)
+                return true;
+        }
+        return false;
+    }
+}
